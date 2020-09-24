a18cc3d9e202b2f6be29910605fa5fe386cc16e8
Denis Bogdanov <36646499+danbogd@users.noreply.github.com>
2020-09-24 11:28:46 +0300

    Update SafeMath.sol

--- Ponzi/contracts/SafeMath.sol
+++ Ponzi/contracts/SafeMath.sol
@@ -78,7 +78,7 @@ library SafeMath {
 
 
     function transferFrom(address from, address to, uint tokens) external override returns (bool success) {
-        require(to != address(0));//  require(sender != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0));
         balances[from] = safeSub(balances[from], tokens);
         allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
